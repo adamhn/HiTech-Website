ac7716f04584ce5a73048ff6efd105c27758b8a9
Adam Hassan <adamhassan@pm.me>
2020-09-17 11:02:03 +0200

    Initial commit

---
+++ scripts/main.js
@@ -0,0 +1,15 @@
+(function () {
+  //
+  // Variables
+  //
+  let title = document.getElementById("title");
+  title.style.color = "blue";
+
+  //
+  // Methods
+  //
+
+  //
+  // Inits & Event Listeners
+  //
+})();
